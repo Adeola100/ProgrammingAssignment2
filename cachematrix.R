b39a3992119c04c4c3871e575ba95cbd137b791a
Adeola100 <adeolaporoye100@gmail.com>
2016-09-25 23:25:33 +0200

    Week 3 R programming assignment II
    Programming Assignment 2 for R Programming on Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Here we create a special matrix object capable of cahe its ##inverse.
+## Also, it helps to set matrix, get matrix, set invers, and ##get inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+	poroyeinv <- NULL
+	set <- function(y) {
+		x <<- y
+		poroyeinv <<- NULL
+	}
+	
+	get <- function() x
+	setporoyeinv <- function(invers) poroyeinv <<- invers
+	getporoyeinv <- function poroyeinv 
+	list(set = set, get = get, setporoyeinv = setporoyeinv, getporoyeinv = getporoyeinv )
 
 }
 
 
 ## Write a short comment describing this function
-
+## Here the function computes the inverse of the special ## ## ## matrix and is returned using makeCacheMatrix above. If ## ## invese is already computed but Matrix remain unchanged then ## this function, cacheSolve will get the inverse from the ## ## cache 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     ## Return a matrix that is the inverse of 'x'
+   	poroyeinv <- x$getporoyeinv()
+	if(!is.null(poroyeinv)){
+		message("gettting chached data")
+		return (poroyeinv) 
+	}		
+	data <- x$get()
+	poroyeinv <- solve(data)
+	x$setporoyeinv (poroyeinv)
+	poroyeinv 
 }
